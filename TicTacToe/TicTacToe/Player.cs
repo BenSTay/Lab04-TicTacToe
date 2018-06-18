e65da14c150486e1b9e734fa70218ba122bae29e
Benjamin Taylor <staylor.ben@gmail.com>
2018-06-17 23:03:47 -0700

    Finished TicTacToe lab.

---
+++ TicTacToe/TicTacToe/Player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToe
+{
+    public class Player
+    {
+        public string Marker { get; set; }
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Creates a new instance of the Player class.
+        /// </summary>
+        /// <param name="marker">The player's marker.</param>
+        public Player(string marker)
+        {
+            Marker = marker;
+        }
+    }
+}
---
+++ TicTacToe/TicTacToe/Square.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToe
+{
+    public class Square
+    {
+        public string Content { get; set; }
+
+        /// <summary>
+        /// Creates a new instance of the Square class.
+        /// </summary>
+        /// <param name="content">The content of the square.</param>
+        public Square(string content)
+        {
+            Content = content;
+        }
+    }
+}
